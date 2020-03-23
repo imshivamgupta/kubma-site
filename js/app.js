3a36dc63f7a34af68156fad0d6b7011ecbe89d10
shivam gupta <shivamg8797@gmail.com>
2020-03-23 12:20:58 +0530

    form comment box removed

--- js/app.js
+++ js/app.js
@@ -122,6 +122,7 @@
     $("html, body").animate({ scrollTop: 0 }, 3000);
     return false;
   });
+  // Scroll to booking
   $(".btn-light").click(function() {
     let val = $(document).width() <= 400 ? 5376 : 2580;
     $("html, body").animate({ scrollTop: val }, 3000);
