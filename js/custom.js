cd3a4b4eb3ca43ff9bc799017601d328e39e2e51
shivam gupta <shivamg8797@gmail.com>
2020-04-23 19:11:58 +0530

    added new member

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,32 @@
-API = 'https://kubma.com'
+API = "https://kubma.com";
 $(function () {
-	$('#contact-form').on('submit', function (event) {
-		event.preventDefault()
-		$('#submit').attr('disabled', true);
-		$('#loader').show()
-		$('#label').hide()
-		$.ajax({
-			type: 'post',
-			url: API + '/contact',
-			data: {
-				name: $('#name').val(),
-				email: $('#email').val(),
-				mobile: $('#mobile').val(),
-				message: $('#comments').val()
-			}
-		}).then(function (res) {
-			if (res.status) {
-				$('#simple-msg').show()
-				$('#simple-msg').fadeOut(3000)
-				$('#submit').attr('disabled', false);
-				$('#loader').hide()
-				$('#label').show()
-				$('#name').val('')
-				$('#email').val('')
-				$('#comments').val('')
-				$('#mobile').val('')
-			}
-		})
-	})
-})
+  $("#contact-form").on("submit", function (event) {
+    event.preventDefault();
+    $("#submit").attr("disabled", true);
+    $("#loader").show();
+    $("#label").hide();
+    $.ajax({
+      type: "post",
+      url: API + "/contact",
+      data: {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        mobile: $("#mobile").val(),
+        // message: $('#comments').val()
+      },
+    }).then(function (res) {
+      if (res.status) {
+        $("#simple-msg").show();
+        $("#simple-msg").fadeOut(3000);
+        $("#submit").attr("disabled", false);
+        $("#loader").hide();
+        $("#label").show();
+        // $("#name").val("");
+        // $("#email").val("");
+        // $("#comments").val("");
+        // $("#mobile").val("");
+        $(this).get(0).reset();
+      }
+    });
+  });
+});
