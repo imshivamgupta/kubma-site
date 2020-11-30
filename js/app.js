ae29b48d7a01d0314698269af5fb5a1145442bfc
shivam gupta <shivamg8797@gmail.com>
2020-11-30 16:53:31 +0530

    checking for phone

--- js/app.js
+++ js/app.js
@@ -106,25 +106,23 @@
 	// 	items: 1
 	// })
 
-	// function HeroSection() {
-	// 	// Portraint Primary 0
-	// 	// Landscape Primary 90
-	// 	if (!window.screen.orientation.angle) {
-	// 		$('li.bg-slider').css({
-	// 			height: `${portrait.height}px`
-	// 		})
-	// 		console.log(portrait.height)
-	// 	} else {
-	// 		$('li.bg-slider').css({
-	// 			height: `${landscape.height}px`
-	// 		})
-	// 		console.log(landscape.height)
-	// 	}
-	// }
-	// HeroSection()
-	// $(window).resize(() => {
-	// 	HeroSection()
-	// })
+	function HeroSection() {
+		// Portraint Primary 0
+		// Landscape Primary 90
+		if (!window.screen.orientation.angle) {
+			$('li.bg-slider').css({
+				height: `${window.innerHeight - 80}px`
+			})
+		} else {
+			$('li.bg-slider').css({
+				height: `${window.innerHeight - 80}px`
+			})
+		}
+	}
+	HeroSection()
+	$(window).resize(() => {
+		HeroSection()
+	})
 })(jQuery)
 
 // For sorting team section
