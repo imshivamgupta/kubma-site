1da1eed6820d2ee8e9ae61767d3823744d24d4a2
shivam gupta <shivamg8797@gmail.com>
2020-11-30 15:03:52 +0530

    resize problem

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-/* Template Name: Landrick - Saas & Software Landing Page Template
-   Author: Shreethemes
-   E-mail: shreethemes@gmail.com
-   Version: 2.0.0
-   Created: August 2019
-   File Description: Main JS file of the template
-*/
-
 /****************************/
 /*         INDEX            */
 /*===========================
@@ -16,107 +8,113 @@
  ===========================*/
 
 !(function ($) {
-  'use strict'
-  // Loader
-  $(window).on('load', function () {
-    $('#status').fadeOut()
-    $('#preloader').delay(350).fadeOut('slow')
-    $('body').delay(350).css({
-      overflow: 'visible',
-    })
-  })
+	'use strict'
+	// Loader
+	$(window).on('DOMContentLoaded', function () {
+		$('#status').fadeOut()
+		$('#preloader').fadeOut('slow')
+		$('body').css({
+			overflow: 'visible'
+		})
+	})
 
-  // Menu
-  $('.navbar-toggle').on('click', function (event) {
-    $(this).toggleClass('open')
-    $('#navigation').slideToggle(400)
-  })
+	// Menu
+	$('.navbar-toggle').on('click', function (event) {
+		$(this).toggleClass('open')
+		$('#navigation').slideToggle(400)
+	})
 
-  $('.navigation-menu>li').slice(-1).addClass('last-elements')
+	$('.navigation-menu>li').slice(-1).addClass('last-elements')
 
-  $('.menu-arrow,.submenu-arrow').on('click', function (e) {
-    if ($(window).width() < 992) {
-      e.preventDefault()
-      $(this)
-        .parent('li')
-        .toggleClass('open')
-        .find('.submenu:first')
-        .toggleClass('open')
-    }
-  })
+	$('.menu-arrow,.submenu-arrow').on('click', function (e) {
+		if ($(window).width() < 992) {
+			e.preventDefault()
+			$(this)
+				.parent('li')
+				.toggleClass('open')
+				.find('.submenu:first')
+				.toggleClass('open')
+		}
+	})
 
-  $('.navigation-menu a').each(function () {
-    if (this.href == window.location.href) {
-      $(this).parent().addClass('active')
-      $(this).parent().parent().parent().addClass('active')
-      $(this).parent().parent().parent().parent().parent().addClass('active')
-    }
-  })
+	$('.navigation-menu a').each(function () {
+		if (this.href == window.location.href) {
+			$(this).parent().addClass('active')
+			$(this).parent().parent().parent().addClass('active')
+			$(this).parent().parent().parent().parent().parent().addClass('active')
+		}
+	})
 
-  // Clickable Menu
-  $('.has-submenu a').click(function () {
-    if (window.innerWidth < 992) {
-      if ($(this).parent().hasClass('open')) {
-        $(this).siblings('.submenu').removeClass('open')
-        $(this).parent().removeClass('open')
-      } else {
-        $(this).siblings('.submenu').addClass('open')
-        $(this).parent().addClass('open')
-      }
-    }
-  })
+	// Clickable Menu
+	$('.has-submenu a').click(function () {
+		if (window.innerWidth < 992) {
+			if ($(this).parent().hasClass('open')) {
+				$(this).siblings('.submenu').removeClass('open')
+				$(this).parent().removeClass('open')
+			} else {
+				$(this).siblings('.submenu').addClass('open')
+				$(this).parent().addClass('open')
+			}
+		}
+	})
 
-  $('.mouse-down').on('click', function (event) {
-    var $anchor = $(this)
-    $('html, body')
-      .stop()
-      .animate(
-        {
-          scrollTop: $($anchor.attr('href')).offset().top - 0,
-        },
-        1500,
-        'easeInOutExpo'
-      )
-    event.preventDefault()
-  })
+	$('.mouse-down').on('click', function (event) {
+		var $anchor = $(this)
+		$('html, body')
+			.stop()
+			.animate(
+				{
+					scrollTop: $($anchor.attr('href')).offset().top - 0
+				},
+				1500,
+				'easeInOutExpo'
+			)
+		event.preventDefault()
+	})
 
-  // Back to top
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn()
-    } else {
-      $('.back-to-top').fadeOut()
-    }
-  })
-  $('.back-to-top').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 3000)
-    return false
-  })
+	// Back to top
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.back-to-top').fadeIn()
+		} else {
+			$('.back-to-top').fadeOut()
+		}
+	})
+	$('.back-to-top').click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 3000)
+		return false
+	})
 
-  //Cta Video
-  $('.video-play-icon').magnificPopup({
-    disableOn: 375,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false,
-  })
-  //Owl Carousel
-  $('#owl-fade').owlCarousel({
-    loop: true,
-    nav: false,
-    dots: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    animateOut: 'fadeOut',
-    items: 1,
-  })
+	//Cta Video
+	// $('.video-play-icon').magnificPopup({
+	// 	disableOn: 375,
+	// 	type: 'iframe',
+	// 	mainClass: 'mfp-fade',
+	// 	removalDelay: 160,
+	// 	preloader: false,
+	// 	fixedContentPos: false
+	// })
+	//Owl Carousel
+	// $('#owl-fade').owlCarousel({
+	// 	loop: true,
+	// 	nav: false,
+	// 	dots: true,
+	// 	autoplay: true,
+	// 	autoplayTimeout: 3000,
+	// 	autoplayHoverPause: true,
+	// 	animateOut: 'fadeOut',
+	// 	items: 1
+	// })
 
-  $('li.bg-slider').css({
-    height: `${document.documentElement.clientHeight}px`,
-  })
+	// function HeroSection() {
+	// 	$('li.bg-slider').css({
+	// 		height: `${document.documentElement.clientHeight}px`
+	// 	})
+	// }
+	// HeroSection()
+	// $(window).resize(() => {
+	// 	HeroSection()
+	// })
 })(jQuery)
 
 // For sorting team section
--- js/three.js
+++ js/three.js
@@ -5,205 +5,205 @@ import * as THREE from 'https://unpkg.com/three@0.121.1/build/three.module.js'
 var camera, scene, renderer, material
 
 var isUserInteracting = false,
-  onPointerDownMouseX = 0,
-  onPointerDownMouseY = 0,
-  lon = 0,
-  onPointerDownLon = 0,
-  lat = 0,
-  onPointerDownLat = 0,
-  phi = 0,
-  theta = 0
+	onPointerDownMouseX = 0,
+	onPointerDownMouseY = 0,
+	lon = 0,
+	onPointerDownLon = 0,
+	lat = 0,
+	onPointerDownLat = 0,
+	phi = 0,
+	theta = 0
 
 init()
 animate()
 
 function init() {
-  var container, mesh
+	var container, mesh
 
-  container = document.getElementById('particle')
+	container = document.getElementById('particle')
 
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    1,
-    1100
-  )
-  camera.target = new THREE.Vector3(0, 0, 0)
+	camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		1,
+		1100
+	)
+	camera.target = new THREE.Vector3(0, 0, 0)
 
-  scene = new THREE.Scene()
+	scene = new THREE.Scene()
 
-  var geometry = new THREE.SphereBufferGeometry(500, 60, 40)
-  // invert the geometry on the x-axis so that all of the faces point inward
-  geometry.scale(-1, 1, 1)
+	var geometry = new THREE.SphereBufferGeometry(500, 60, 40)
+	// invert the geometry on the x-axis so that all of the faces point inward
+	geometry.scale(-1, 1, 1)
 
-  //cubemap
-  var loader = new THREE.CubeTextureLoader()
-  loader.setPath('cube/')
+	//cubemap
+	var loader = new THREE.CubeTextureLoader()
+	loader.setPath('cube/')
 
-  var textureCube = loader.load([
-    'px.jpg',
-    'nx.jpg',
-    'py.jpg',
-    'ny.jpg',
-    'pz.jpg',
-    'nz.jpg',
-  ])
+	var textureCube = loader.load([
+		'px.jpg',
+		'nx.jpg',
+		'py.jpg',
+		'ny.jpg',
+		'pz.jpg',
+		'nz.jpg'
+	])
 
-  material = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    envMap: textureCube,
-  })
+	material = new THREE.MeshBasicMaterial({
+		color: 0xffffff,
+		envMap: textureCube
+	})
 
-  mesh = new THREE.Mesh(geometry, material)
+	mesh = new THREE.Mesh(geometry, material)
 
-  scene.add(mesh)
+	scene.add(mesh)
 
-  renderer = new THREE.WebGLRenderer()
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  container.appendChild(renderer.domElement)
+	renderer = new THREE.WebGLRenderer()
+	renderer.setPixelRatio(window.devicePixelRatio)
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	container.appendChild(renderer.domElement)
 
-  container.style.touchAction = 'none'
-  container.addEventListener('pointerdown', onPointerDown, false)
+	container.style.touchAction = 'none'
+	container.addEventListener('pointerdown', onPointerDown, false)
 
-  // document.addEventListener('wheel', onDocumentMouseWheel, false)
+	// document.addEventListener('wheel', onDocumentMouseWheel, false)
 
-  //
+	//
 
-  document.addEventListener(
-    'dragover',
-    function (event) {
-      event.preventDefault()
-      event.dataTransfer.dropEffect = 'copy'
-    },
-    false
-  )
+	document.addEventListener(
+		'dragover',
+		function (event) {
+			event.preventDefault()
+			event.dataTransfer.dropEffect = 'copy'
+		},
+		false
+	)
 
-  document.addEventListener(
-    'dragenter',
-    function () {
-      document.body.style.opacity = 0.5
-    },
-    false
-  )
+	document.addEventListener(
+		'dragenter',
+		function () {
+			document.body.style.opacity = 0.5
+		},
+		false
+	)
 
-  document.addEventListener(
-    'dragleave',
-    function () {
-      document.body.style.opacity = 1
-    },
-    false
-  )
+	document.addEventListener(
+		'dragleave',
+		function () {
+			document.body.style.opacity = 1
+		},
+		false
+	)
 
-  document.addEventListener(
-    'drop',
-    function (event) {
-      event.preventDefault()
+	document.addEventListener(
+		'drop',
+		function (event) {
+			event.preventDefault()
 
-      var reader = new FileReader()
-      reader.addEventListener(
-        'load',
-        function (event) {
-          material.map.image.src = event.target.result
-          material.map.needsUpdate = true
-        },
-        false
-      )
-      reader.readAsDataURL(event.dataTransfer.files[0])
+			var reader = new FileReader()
+			reader.addEventListener(
+				'load',
+				function (event) {
+					material.map.image.src = event.target.result
+					material.map.needsUpdate = true
+				},
+				false
+			)
+			reader.readAsDataURL(event.dataTransfer.files[0])
 
-      document.body.style.opacity = 1
-    },
-    false
-  )
+			document.body.style.opacity = 1
+		},
+		false
+	)
 
-  //
+	//
 
-  window.addEventListener('resize', onWindowResize, false)
+	window.addEventListener('resize', onWindowResize, false)
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
 
-  renderer.setSize(window.innerWidth, window.innerHeight)
+	renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
 function onPointerDown(event) {
-  if (event.isPrimary === false) return
+	if (event.isPrimary === false) return
 
-  isUserInteracting = true
+	isUserInteracting = true
 
-  onPointerDownMouseX = event.clientX
-  onPointerDownMouseY = event.clientY
+	onPointerDownMouseX = event.clientX
+	onPointerDownMouseY = event.clientY
 
-  onPointerDownLon = lon
-  onPointerDownLat = lat
+	onPointerDownLon = lon
+	onPointerDownLat = lat
 
-  document.addEventListener('pointermove', onPointerMove, false)
-  document.addEventListener('pointerup', onPointerUp, false)
+	document.addEventListener('pointermove', onPointerMove, false)
+	document.addEventListener('pointerup', onPointerUp, false)
 }
 
 function onPointerMove(event) {
-  if (event.isPrimary === false) return
+	if (event.isPrimary === false) return
 
-  lon = (onPointerDownMouseX - event.clientX) * 0.1 + onPointerDownLon
-  lat = (event.clientY - onPointerDownMouseY) * 0.1 + onPointerDownLat
+	lon = (onPointerDownMouseX - event.clientX) * 0.1 + onPointerDownLon
+	lat = (event.clientY - onPointerDownMouseY) * 0.1 + onPointerDownLat
 }
 
 function onPointerUp() {
-  if (event.isPrimary === false) return
+	if (event.isPrimary === false) return
 
-  isUserInteracting = false
+	isUserInteracting = false
 
-  document.removeEventListener('pointermove', onPointerMove)
-  document.removeEventListener('pointerup', onPointerUp)
+	document.removeEventListener('pointermove', onPointerMove)
+	document.removeEventListener('pointerup', onPointerUp)
 }
 
 function onDocumentMouseWheel(event) {
-  var fov = camera.fov + event.deltaY * 0.05
+	var fov = camera.fov + event.deltaY * 0.05
 
-  camera.fov = THREE.MathUtils.clamp(fov, 10, 75)
+	camera.fov = THREE.MathUtils.clamp(fov, 10, 75)
 
-  camera.updateProjectionMatrix()
+	camera.updateProjectionMatrix()
 }
 
 function animate() {
-  requestAnimationFrame(animate)
-  update()
+	requestAnimationFrame(animate)
+	update()
 }
 
 function update() {
-  if (isUserInteracting === false) {
-    lon += 0.1
-  }
+	if (isUserInteracting === false) {
+		lon += 0.1
+	}
 
-  lat = Math.max(-85, Math.min(85, lat))
-  phi = THREE.MathUtils.degToRad(90 - lat)
-  theta = THREE.MathUtils.degToRad(lon)
+	lat = Math.max(-85, Math.min(85, lat))
+	phi = THREE.MathUtils.degToRad(90 - lat)
+	theta = THREE.MathUtils.degToRad(lon)
 
-  camera.target.x = 500 * Math.sin(phi) * Math.cos(theta)
-  camera.target.y = 500 * Math.cos(phi)
-  camera.target.z = 500 * Math.sin(phi) * Math.sin(theta)
+	camera.target.x = 500 * Math.sin(phi) * Math.cos(theta)
+	camera.target.y = 500 * Math.cos(phi)
+	camera.target.z = 500 * Math.sin(phi) * Math.sin(theta)
 
-  camera.lookAt(camera.target)
+	camera.lookAt(camera.target)
 
-  /*
+	/*
     // distortion
     camera.position.copy( camera.target ).negate();
     */
-  // watchCounter++
-  // if (watchCounter > 100) {
-  //   watchCounter = 0
-  //   material.color.setHex(getRandomColor())
-  // }
-  renderer.render(scene, camera)
+	// watchCounter++
+	// if (watchCounter > 100) {
+	//   watchCounter = 0
+	//   material.color.setHex(getRandomColor())
+	// }
+	renderer.render(scene, camera)
 }
 var watchCounter = 0
 function getRandomColor() {
-  var letters = '0123456789ABCDEF'
-  let color = '0x'
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
-  }
-  return parseInt(color)
+	var letters = '0123456789ABCDEF'
+	let color = '0x'
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)]
+	}
+	return parseInt(color)
 }
